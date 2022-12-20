7ae9e2bda6023ebfc0a50ded379e0f84107ac8f5
Super-Jinx <loislen95@gmail.com>
2022-12-20 11:43:13 -0400

    Operadores-e-Metodos

---
+++ tipos-variaveis/src/Usuario.java
@@ -0,0 +1,14 @@
+
+public class Usuario {
+    public static void main(String[] args) throws Exception{
+        
+        SmartTv smartTv = new SmartTv();
+
+ System.out.println("Tv Ligada? :"+ smartTv.ligada);
+ System.out.println("Canal Atual? :"+ smartTv.canal);
+ System.out.println("Volume Atual? :"+ smartTv.volume);  
+
+
+}   
+    
+}
